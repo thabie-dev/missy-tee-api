71abf37e001dd1c06dfc4fc11c750ab5e1454223
thabie-dev <taniatdube1@gmail.com>
2022-04-29 23:52:22 +0200

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // add code in here to create an API with ExpressJS
 const express = require('express');
-const cors =require('cors')
+const cors = require('cors')
 const app = express();
+const jwt = require('jsonwebtoken')
+const fs = require('fs')
 // import the dataset to be used here
 const garments = require('./garments.json');
 
@@ -19,105 +21,144 @@ app.use(cors())
 //     res.json({garments});
 // });
 
+function checkToken(req, res, next) {
 
-app.get('/api/garments', function(req, res){
+    const token =req.params.token
+    console.log(token)
+    // const token = req.body.token & req.body.token.split(" ")[1];
 
+    console.log(req.body.token);
+
+    // if (!req.headers.authorization || !token){
+    // 	res.sendStatus(401);
+    // 	return;
+    // }
+
+    // what do I need to do with the token ?
+
+    // unwrap the decode the token...
+    const decoded = jwt.verify(token, 'QueenPresident@Mmabatho');
+
+    // find the username in the token ?
+    const { username } = decoded;
+
+    console.log(username);
+
+    // check if the username in the token is 'avermeulen'
+    if (username && username === 'President') {
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+
+}
+
+app.get('/api/garments', function (req, res) {
+
+    const token =req.params.token
+    console.log(token)
     const gender = req.query.gender;
     const season = req.query.season;
 
     const filteredGarments = garments.filter(garment => {
         // if both gender & season was supplied
         if (gender != 'All' && season != 'All') {
-            return garment.gender === gender 
-                && garment.season === season;
-        } else if(gender != 'All') { // if gender was supplied
             return garment.gender === gender
-        } else if(season != 'All') { // if season was supplied
+                && garment.season === season;
+        } else if (gender != 'All') { // if gender was supplied
+            return garment.gender === gender
+        } else if (season != 'All') { // if season was supplied
             return garment.season === season
         }
         return true;
     });
+});
 
-    // note that this route just send JSON data to the browser
-    // there is no template
-    res.json({ 
-        garments : filteredGarments
+app.get('/api/login', function (req, res) {
+    var token = jwt.sign({ username: 'President'}, 'QueenPresident@Mmabatho');
+    console.log(token)
+
+    var decoded = jwt.decode(token);
+    console.log(decoded)
+
+    res.json({
+         token
     });
 });
 
 
-    // app.get('/api/garments/price/:price', function(req, res){
-    // const maxPrice = Number(req.params.price);
-    // const filteredGarments = garments.filter( garment => {
-    //     // filter only if the maxPrice is bigger than maxPrice
-    //     if (maxPrice > 0) {
-    //         return garment.price <= maxPrice;
-    //     }
-    //     return true;
-    // });
 
-    app.get('/api/garments/price/:price', function(req, res){
-        const maxPrice = Number(req.params.price);
-        const filteredGarments = garments.filter( garment => {
-            // filter only if the maxPrice is bigger than maxPrice
-            if (maxPrice > 0) {
-                return garment.price <= maxPrice;
-            }
-            return true;
-        });
-    
-        res.json({ 
-            garments : filteredGarments
-        });
+// note that this route just send JSON data to the browser
+// there is no template
+
+
+
+
+// app.get('/api/garments/price/:price', function(req, res){
+// const maxPrice = Number(req.params.price);
+// const filteredGarments = garments.filter( garment => {
+//     // filter only if the maxPrice is bigger than maxPrice
+//     if (maxPrice > 0) {
+//         return garment.price <= maxPrice;
+//     }
+//     return true;
+// });
+
+app.get('/api/garments/price/:price', function (req, res) {
+    const maxPrice = Number(req.params.price);
+    const filteredGarments = garments.filter(garment => {
+        // filter only if the maxPrice is bigger than maxPrice
+        if (maxPrice > 0) {
+            return garment.price <= maxPrice;
+        }
+        return true;
     });
 
-    app.post('/api/garments', (req, res) => {
+    res.json({
+        garments: filteredGarments
+    });
+});
 
-        // get the fields send in from req.body
-        const {
+app.post('/api/garments', (req, res) => {
+
+    // get the fields send in from req.body
+    const {
+        description,
+        img,
+        gender,
+        season,
+        price
+    } = req.body;
+
+    // add some validation to see if all the fields are there.
+    // only 3 fields are made mandatory here
+    // you can change that
+
+    if (!description || !img || !price) {
+        res.json({
+            status: 'error',
+            message: 'Required data not supplied',
+        });
+    } else {
+
+        // you can check for duplicates here using garments.find
+
+        // add a new entry into the garments list
+        garments.push({
             description,
             img,
             gender,
             season,
             price
-        } = req.body;
-    
-        // add some validation to see if all the fields are there.
-        // only 3 fields are made mandatory here
-        // you can change that
-    
-        if (!description || !img || !price) {
-            res.json({
-                status: 'error',
-                message: 'Required data not supplied',
-            });
-        } else {
-    
-            // you can check for duplicates here using garments.find
-            
-            // add a new entry into the garments list
-            garments.push({
-                description,
-                img,
-                gender,
-                season,
-                price
-            });
-    
-            res.json({
-                status: 'success',
-                message: 'New garment added.',
-            });
-        }
-    
-    });
-    
-       
+        });
 
+        res.json({
+            status: 'success',
+            message: 'New garment added.',
+        });
+    }
 
-
-
-
+});
 
 
 
@@ -136,7 +177,18 @@ app.get('/api/garments', function(req, res){
 // });
 
 const PORT = process.env.PORT || 4017;
-app.listen(PORT, function() {
-console.log(`App started on port ${PORT}`)
-       
+app.listen(PORT, function () {
+    console.log(`App started on port ${PORT}`)
+
 });
+// var privateKey = fs.readFileSync('private.key');
+// jwt.sign({ foo: 'garments' }, privateKey, { algorithm: 'RS256' }, function(err, token) {
+//     console.log(token);
+//   });
+
+
+var token = jwt.sign({ foo: 'garments' }, 'shhhhh');
+console.log(token)
+
+var decoded = jwt.decode(token);
+console.log(decoded)
--- public/app.js
+++ public/app.js
@@ -10,56 +10,62 @@ const showPriceRangeElem = document.querySelector('.showPriceRange');
 const garmentsTemplateText = document.querySelector('.garmentListTemplate');
 const garmentsTemplate = Handlebars.compile(garmentsTemplateText.innerHTML);
 
-seasonOptions.addEventListener('click', function(evt){
+seasonOptions.addEventListener('click', function (evt) {
 	seasonFilter = evt.target.value;
 	filterData();
 });
 
-genderOptions.addEventListener('click', function(evt){
+genderOptions.addEventListener('click', function (evt) {
 	genderFilter = evt.target.value;
 	filterData();
 });
 
-function garments(){
+function garments() {
 	axios
-	.get(`http://localhost:4017/api/garments`, function(result){
-		console.log(result.data)
-		searchResultsElem.innerHTML = garmentsTemplate({
-			garments : result.data.garments
+		.get(`http://localhost:4017/api/garments`, function (result) {
+			console.log(result)
+			searchResultsElem.innerHTML = garmentsTemplate({
+				garments: result.data.garments
+			});
 		});
-	});
 }
 
 garments()
 
-function priceGarments(){
+function priceGarments() {
 	axios
- 	.get(`http://localhost:4017/api/garments/price/:price`, function(result){
-		console.log(result.data)
-		searchResultsElem.innerHTML = garmentsTemplate({
-			garments : result.data.garments
+		.get(`http://localhost:4017/api/garments/price/:price`, function (result) {
+			console.log(result.data)
+			searchResultsElem.innerHTML = garmentsTemplate({
+				garments: result.data.garments
+			});
 		});
-	});
 }
 
 function filterData() {
 	axios
-		.get(`/api/garments?gender=${genderFilter}&season=${seasonFilter}`)
-		.then(function(result) {
+		.get(`/api/garments?gender=${genderFilter}&season=${seasonFilter}`
+			// , {
+			// headers: {
+			//   'Authorization': `token ${access_token}`
+			// }
+			//  } 
+		)
+		.then(function (result) {
 			searchResultsElem.innerHTML = garmentsTemplate({
-				garments : result.data.garments
+				garments: result.data.garments
 			})
 		});
 }
 
-priceRangeElem.addEventListener('change', function(evt){
+priceRangeElem.addEventListener('change', function (evt) {
 	const maxPrice = evt.target.value;
 	showPriceRangeElem.innerHTML = maxPrice;
 	axios
 		.get(`/api/garments/price/${maxPrice}`)
-		.then(function(result) {
+		.then(function (result) {
 			searchResultsElem.innerHTML = garmentsTemplate({
-				garments : result.data.garments
+				garments: result.data.garments
 			})
 		});
 });
